81ab191987a5a2a2ddb1d5066c968487ccf44a5d
Katakun <k4t4kun@gmail.com>
2021-12-13 16:15:45 +0900

    before stage 5

--- Game.java
+++ Game.java
@@ -14,47 +14,79 @@ public class Game {
 
     public void start() {
         myField.printField();
+        Ship[] ships = new Ship[5]; // массив всех кораблей
 
-        placeShip("Aircraft Carrier", 5);
+        Ship aircraftCarrier = placeShip("Aircraft Carrier", 5);
+        ships[0] = aircraftCarrier;
         myField.printField();
 
-        placeShip("Battleship", 4);
+        Ship battleship = placeShip("Battleship", 4);
+        ships[1] = battleship;
         myField.printField();
 
-        placeShip("Submarine", 3);
+        Ship submarine = placeShip("Submarine", 3);
+        ships[2] = submarine;
         myField.printField();
 
-        placeShip("Cruiser", 3);
+        Ship cruiser =  placeShip("Cruiser", 3);
+        ships[3] = cruiser;
         myField.printField();
 
-        placeShip("Destroyer", 2);
+        Ship destroyer = placeShip("Destroyer", 2);
+        ships[4] = destroyer;
         myField.printField();
 
-        System.out.println("\nThe game starts!\n");
-        myField.printField();
+        System.out.println("\nThe game starts!");
 
-        int[] shotPoint = shotPoint();
-        shotIndicate(shotPoint);
+        myField.printFogField();
+
+        while (myField.hasShipAfloat()) { // пока хоть один корабль на плаву
+            int[] shotPoint = shotPoint(); // Take a shot возвращает координаты
+            shootRegistration(shotPoint);  // Отмечает на карте X или M
+            myField.printFieldWithFogAndShotPoint(); // Выводит карту с туманом войны
+            printHitOrMiss(shotPoint);  // You hit a ship! или You missed!
+
+            // проверка на потопление корабля
+            for (Ship ship : ships) {
+                if (ship.atFloat) { // если корабль на плаву
+                    if (!ship.isShipAtFloat(myField) && myField.hasShipAfloat()) { // если проверка показала что он утонул
+                        System.out.println("You sank a ship! Specify a new target:");
+                    }
+                }
+            }
+
+        }
+
+        System.out.println("You sank the last ship. You won. Congratulations!");
+
+
+
+
 
     }
 
-
-    public void shotIndicate(int[] shotPoint) {
+    public void shootRegistration(int[] shotPoint) {
         int row = shotPoint[0];
         int col = shotPoint[1];
-        if (myField.field[row][col].equals(" O")) {
+        if (myField.field[row][col].equals(" O") || myField.field[row][col].equals(" X") ) {
             myField.field[row][col] = " X";
-            myField.printField();
-            System.out.println("You hit a ship!");
         } else {
             myField.field[row][col] = " M";
-            myField.printField();
-            System.out.println("You missed!");
+        }
+    }
+
+    public void printHitOrMiss(int[] shotPoint) {
+        int row = shotPoint[0];
+        int col = shotPoint[1];
+        if (myField.field[row][col].equals(" X") && myField.hasShipAfloat()) {
+            System.out.println("\nYou hit a ship!");
+        } else if (myField.field[row][col].equals(" M")){
+            System.out.println("\nYou missed!");
         }
     }
 
     public int[] shotPoint() {
-        System.out.println("Take a shot!\n");
+        System.out.println("\nTake a shot!\n");
 
         while (true) {
             String input = scanner.nextLine();
@@ -68,12 +100,12 @@ public class Game {
     }
 
 
-    public void placeShip(String shipType, int shipSize) {
+    public Ship placeShip(String shipType, int shipSize) {
         System.out.printf("\nEnter the coordinates of the %s (%d cells):\n\n", shipType, shipSize);
 
         while (true) {
             String input = scanner.nextLine();
-            Ship ship = new Ship(input);
+            Ship ship = new Ship(input, shipType);
 
             boolean isShipSizeOk = ship.getSize() == shipSize;
             boolean isShipLocationOk = ship.isLocationOk();
@@ -81,7 +113,7 @@ public class Game {
 
             if (isShipSizeOk && isShipLocationOk && isShipPositionOk) {
                 myField.placeShip(ship);
-                break;
+                return ship;
             } else if (!isShipSizeOk) {
                 System.out.printf("\nError! Wrong length of the %s! Try again:\n", shipType);
             } else if (!isShipLocationOk) {
